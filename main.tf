30e5268d55d8ed87e7529036b228594e5d252102
Markosz008 <kardos.mark1991@gmail.com>
2026-05-06 12:18:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  # Ez a blokk mondja meg a Terraformnak, hogy NE helyben tárolja a state-et
+  backend "s3" {
+    bucket         = "markosz-jenkins-terraform-state" # Ide írd a saját bucketed nevét!
+    key            = "projektek/jenkins-vpc/terraform.tfstate"     # A fájl elérési útja a bucketben
+    region         = "eu-central-1"
+    encrypt        = true                              # Biztonság kedvéért titkosítjuk
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,8 +16,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-central-1" # Vagy ahol tegnap építkeztél
-  # NINCS profile = "..." sor!
+  region = "eu-central-1"
 }
 
 resource "aws_vpc" "elso_halozatom" {
