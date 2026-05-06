24db3dd8f310bcb12649d8f85067c61f3ae13eb0
Markosz008 <kardos.mark1991@gmail.com>
2026-05-06 18:00:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -128,10 +128,15 @@ resource "aws_instance" "bastion" {
 resource "aws_instance" "web_szerver" {
   ami                    = data.aws_ami.latest_amazon_linux.id
   instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.private_subnet.id
+  subnet_id              = aws_subnet.public_subnet.id # ÁTVÁLTVA PUBLIKUSRA!
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   key_name               = aws_key_pair.jenkins_key.key_name
-  tags = { Name = "Private-Web-Server" }
+  tags = { Name = "Web-Server-Public-Access" }
+}
+
+# Mivel most már publikus alhálózatban van, lekérhetjük a publikus IP-jét is a böngészőhöz
+output "web_public_ip" {
+  value = aws_instance.web_szerver.public_ip
 }
 
 output "bastion_ip" { value = aws_instance.bastion.public_ip }
