348a9ce3cb9f0348b6a78e04ebe31ae8f8d3c55a
Markosz <markosz@MacBook-Pro.local>
2026-05-07 15:01:04 +0200

    Security: Move passwords to Jenkins credentials and environment variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,7 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.elso_halozatom.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
   tags = { Name = "Public-Subnet-Bastion" }
 }
